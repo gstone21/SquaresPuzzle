351ea468d44da5b5344492f59846340fa8fa51a8
Grant Stone <gwstone@ucsc.edu>
2019-11-10 22:43:04 -0800

    First commit

---
+++ app/src/main/java/com/example/squarespuzzlestoneg21/PuzzleController.java
@@ -0,0 +1,37 @@
+package com.example.squarespuzzlestoneg21;
+
+import android.view.MotionEvent;
+import android.view.View;
+
+public class PuzzleController implements View.OnTouchListener {
+    private PuzzleView puzzle;
+
+    public PuzzleController(PuzzleView v){
+        puzzle = v;
+
+    }
+
+    @Override
+    public boolean onTouch (View v, MotionEvent event){
+        float x = event.getX();
+        float y = event.getY();
+
+        for(Square square : puzzle.squares){
+            if(square.getIntNum() != 16 && square.getLeft() <= x && square.getRight() >= x && square.getTop() <= y && square.getBottom() >= y){
+
+                if(square.nextTo(puzzle.squares[15])){
+                    float tempLeft = square.getLeft();
+                    float tempTop = square.getTop();
+                    square.setCord(puzzle.squares[15].getLeft(), puzzle.squares[15].getTop());
+                    puzzle.squares[15].setCord(tempLeft, tempTop);
+                    puzzle.invalidate();
+                }
+
+            }
+        }
+
+        return true;
+    }
+
+
+}
